ebde0f81eb983851c3913df9006c8c050b7caf6a
Andrei Mateescu <andrei@amateescu.me>
2012-02-10 11:58:31 +0200

    Moved test queues to a API documentation file.

---
+++ entityqueue.api.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by Entityqueue.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * This hook allows modules to provide their own entity queues.
+ *
+ * @return array
+ *   An associative array containing queues passed through entity_import().
+ */
+function hook_entityqueue_default_queues() {
+  $queues = array();
+
+  $queues['featured_articles'] = entity_import('entityqueue',
+    '{
+      "name" : "featured_articles",
+      "type" : "node",
+      "subqueue" : "1",
+      "label" : "Featured articles",
+      "subqueue_label" : "Featured articles",
+      "parent_name" : null,
+      "min_size" : "0",
+      "max_size" : "5",
+      "uid" : "1",
+      "rdf_mapping" : []
+    }');
+
+  return $queues;
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
