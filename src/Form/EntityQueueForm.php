750e9b57aa4744f4b99d04d18cffe24f1c7d7fa0
Andrei Mateescu <andrei@amateescu.me>
2015-10-26 21:07:27 +0200

    Add a confirmation message for the entity queue add and edit forms.

--- src/Form/EntityQueueForm.php
+++ src/Form/EntityQueueForm.php
@@ -9,11 +9,39 @@ namespace Drupal\entityqueue\Form;
 
 use Drupal\Core\Entity\BundleEntityFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Base form for entity queue edit forms.
  */
 class EntityQueueForm extends BundleEntityFormBase {
+
+  /**
+   * A logger instance.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('logger.factory')->get('entityqueue')
+    );
+  }
+
+  /**
+   * Constructs a EntityQueueForm.
+   *
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   */
+  public function __construct(LoggerInterface $logger) {
+    $this->logger = $logger;
+  }
 
   /**
    * {@inheritdoc}
@@ -60,6 +88,26 @@ class EntityQueueForm extends BundleEntityFormBase {
     $entity = parent::buildEntity($form, $form_state);
     $entity->setHandler($entity->get('handler'));
     return $entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $queue = $this->entity;
+    $status = $queue->save();
+
+    $edit_link = $queue->link($this->t('Edit'), 'edit-form');
+    if ($status == SAVED_UPDATED) {
+      drupal_set_message($this->t('The entity queue %label has been updated.', array('%label' => $queue->label())));
+      $this->logger->notice('The entity queue %label has been updated.', array('%label' => $queue->label(), 'link' => $edit_link));
+    }
+    else {
+      drupal_set_message($this->t('The entity queue %label has been added.', array('%label' => $queue->label())));
+      $this->logger->notice('The entity queue %label has been added.', array('%label' => $queue->label(), 'link' =>  $edit_link));
+    }
+
+    $form_state->setRedirectUrl($queue->urlInfo('collection'));
   }
 
 }
